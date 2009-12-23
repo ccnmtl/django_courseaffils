a868a09459f10682f41863e85159bd622d681301
sky <sky@1f418930-7ff8-0310-b8ad-c653122473bc>
2009-12-23 16:07:41 +0000

    moved some lib functions from projects to the right place
    git-svn-id: http://svn.ccnmtl.columbia.edu/django_courseaffils/trunk@18298 1f418930-7ff8-0310-b8ad-c653122473bc

--- courseaffils/lib.py
+++ courseaffils/lib.py
@@ -1,4 +1,7 @@
 from django.db.models.loading import get_model
+from django.template.loader import get_template
+from django.template import Context
+from django.http import Http404
 
 User = get_model('auth', 'User')
 
@@ -7,3 +10,17 @@ def users_in_course(course):
 
 def in_course(user, course):
     return course.group in user.groups
+
+def in_course_or_404(user, group_or_course):
+    "Supports either the course-group or course as second arg"
+    group = getattr(group_or_course,'group', group_or_course)
+    try:
+        return group.user_set.get(username=user)
+    except:
+        template = get_template('not_in_course.html')
+        context = Context({
+                'user': user,
+                'course': group,
+                })
+        response_body = template.render(context)
+        raise Http404(response_body)
