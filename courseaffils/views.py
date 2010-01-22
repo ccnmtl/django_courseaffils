0a2769ed1b8025678bb20a7b3421e63c709eb5de
sky <sky@1f418930-7ff8-0310-b8ad-c653122473bc>
2010-01-22 15:49:39 +0000

    bugfix from prev commit--no urlquote import; also Create a course link for users marked staff
    git-svn-id: http://svn.ccnmtl.columbia.edu/django_courseaffils/trunk@18507 1f418930-7ff8-0310-b8ad-c653122473bc

--- courseaffils/views.py
+++ courseaffils/views.py
@@ -2,15 +2,18 @@ from django.conf import settings
 from django.shortcuts import render_to_response
 from courseaffils.models import Course
 from django.db.models import get_model
-
+from django.utils.http import urlquote
 
 def select_course(request):
     available_courses = Course.objects.filter(group__user=request.user)
+    response_dict = {'request': request,
+                     'user': request.user,
+                     'add_privilege':request.user.is_staff,
+                     }
+
     if len(available_courses) == 0:
         return render_to_response('courseaffils/no_courses.html',
-                                  {'request': request,
-                                   'user': request.user,
-                                   })
+                                  response_dict)
 
     next_redirect = ''
     if request.META.has_key('QUERY_STRING') \
@@ -18,20 +21,11 @@ def select_course(request):
             #just GET (until someone complains)
         next_redirect = '&next=%s' % urlquote(request.get_full_path())
 
-    return render_to_response('courseaffils/select_course.html',
-                              {
+    response_dict.update({
             'courses':available_courses,
-            'user':request.user,
-            'request': request,
             'next':next_redirect,
-            },
-                              )
+            'add_privilege':request.user.is_staff,
+            })
 
-
-def get_mappers(self):
-    mappers = []
-    if not hasattr(settings,'COURSEAFFILS_FACULTY_GROUPS'):
-        return []
-    for mapper_path in settings.WIND_AFFIL_HANDLERS:
-        mappers.append(self.load_handler(mapper_path))
-    return mappers
+    return render_to_response('courseaffils/select_course.html',
+                              response_dict)
