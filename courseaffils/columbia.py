a1c7f8e56513065db553ce2d2a2ac4c6d34276c3
sky <sky@1f418930-7ff8-0310-b8ad-c653122473bc>
2010-01-22 15:26:27 +0000

    create course by course string and allow faculty adding by *
    git-svn-id: http://svn.ccnmtl.columbia.edu/django_courseaffils/trunk@18506 1f418930-7ff8-0310-b8ad-c653122473bc

---
+++ courseaffils/columbia.py
@@ -0,0 +1,120 @@
+import re
+import urllib
+
+class CourseStringMapper:
+    @classmethod
+    def widget(cls):
+        from django import forms
+        w = forms.CharField(required=False,
+                            widget=forms.TextInput,
+                            help_text="""Alternate to choosing a group. Enter the section key from the directory of courses.  e.g. 20101SCNC1000F001 (which is yyyytddddnnnnLsss where y=year, t=term, d=dept, n=course number, L=course letter, s=section) """
+                            )
+        def clean(data):
+            if data:
+                if not SectionkeyTemplate.to_dict(data):
+                    raise forms.ValidationError('Invalid course string.')
+                else:
+                    return data
+        w.clean = clean
+        return w
+
+    @classmethod
+    def get_groups(cls,section_key):
+        from django.contrib.auth.models import Group
+        section_dict = SectionkeyTemplate.to_dict(section_key)
+        if section_dict:
+            stud_grp_name = WindTemplate.to_string(section_dict)
+            fac_grp_name = WindTemplate.to_string(dict(section_dict, member='fc'))
+
+            stud_grp,created = Group.objects.get_or_create(name=stud_grp_name)
+            fac_grp,created = Group.objects.get_or_create(name=fac_grp_name)
+            return stud_grp,fac_grp
+        else:
+            return None,None
+
+    @classmethod
+    def get_course_info(cls,groupname):
+        course_dict = WindTemplate.to_dict(groupname)
+        directory_link = DirectoryLinkTemplate.to_string(course_dict)
+        
+        response = urllib.urlopen(directory_link).read()
+        return DirectoryPageTemplate.to_dict(response)
+
+class WindTemplate:
+    example = 't3.y2007.s001.cw3956.engl.fc.course:columbia.edu'
+
+    @staticmethod
+    def to_string(cdict):
+        cdict['member'] = cdict.get('member','st')
+        return 't%s.y%s.s%s.c%s%s.%s.%s.course:columbia.edu' % (
+            cdict['term'],
+            cdict['year'],
+            cdict['section'],
+            cdict['letter'].lower(),
+            cdict['number'],
+            cdict['dept'].lower(),
+            cdict['member'].lower(),
+            )
+
+    @staticmethod
+    def to_dict(wind_string):
+         wind_match = re.match('t(?P<term>\d).y(?P<year>\d{4}).s(?P<section>\d{3}).c(?P<letter>\w)(?P<number>\d{4}).(?P<dept>\w{4}).(?P<member>\w\w).course:columbia.edu',wind_string)
+         if wind_match:
+             return wind_match.groupdict()
+
+class SectionkeyTemplate:
+    example = '20101SCNC1000F001'
+
+    @staticmethod
+    def to_dict(sectionkey):
+        key_match = re.match('(?P<year>\d{4})(?P<term>\d)(?P<dept>\w{4})(?P<number>\d{4})(?P<letter>\w)(?P<section>\d{3})',sectionkey)
+        if key_match:
+            return key_match.groupdict()
+
+
+class DirectoryLinkTemplate:
+    example = 'http://www.columbia.edu/cu/bulletin/uwb/subj/SCNC/C1100-20101-003/'
+    @staticmethod
+    def to_string(cdict):
+        return 'http://www.columbia.edu/cu/bulletin/uwb/subj/%s/%s%s-%s%s-%s/' % (
+            cdict['dept'].upper(),
+            cdict['letter'].upper(),
+            cdict['number'],
+            cdict['year'],
+            cdict['term'],
+            cdict['section'],
+            )
+        
+class DirectoryPageTemplate:
+    @staticmethod
+    def to_dict(html_page):
+        ret_val = { }
+        look_for = (
+            'Call Number</td>[^>]*>(?P<call_number>\d*)</td>',
+            'Location</td>[^>]*>(?P<times>[^<]*)<br>(?P<location>[^<]*)</td>',
+            'Points</td>[^>]*>(?P<points>[\d-.]*)</td>',
+            'Campus</td>[^>]*>(?P<campus>\w*)</td>',
+            #'approvals_required','type','enrollment','max_enrollment','status',
+            #'title','open_to','note','sectionkey',
+            )
+        for reg in look_for:
+            m = re.search(reg,html_page)
+            if m: ret_val.update( m.groupdict() )
+
+        if ret_val.has_key('times'):
+            m = re.match('(?P<days>\w*)\s*(?P<starttime>[\d:apm]*)-(?P<endttime>[\d:apm]*)', 
+                         ret_val['times'])
+            if m: ret_val.update( m.groupdict() )
+
+        return ret_val
+
+class HashTagTemplate:
+    example = '#CUviet1201'
+
+    @staticmethod
+    def to_string(cdict):
+        return '#CU%s%s' % (
+            cdict['dept'].lower(),
+            cdict['number'],
+            )
+
--- courseaffils/forms.py
+++ courseaffils/forms.py
@@ -1,15 +1,20 @@
 from django import forms
 from django.contrib.auth.models import User,Group
+from django.conf import settings
 from courseaffils.models import Course
 
 class CourseAdminForm(forms.ModelForm):
     class Meta:
         model = Course
-    
+
+    if hasattr(settings,'COURSEAFFILS_COURSESTRING_MAPPER'):
+         course_string = settings.COURSEAFFILS_COURSESTRING_MAPPER.widget()
+
     add_user = forms.CharField(
         required=False,
         widget=forms.Textarea,
-        label="Add users to group (one per line)"
+        label="Add users to group (one per line)",
+        help_text="Put a [ * ] in front of the username to make them faculty.",
         )
 
     users_to_remove = forms.ModelMultipleChoiceField(
@@ -21,11 +26,22 @@ class CourseAdminForm(forms.ModelForm):
 
     def __init__(self, *args, **kw):
         forms.ModelForm.__init__(self, *args, **kw)
-        field = self.fields['users_to_remove']
         if self.instance.user_set:
-            field.queryset = field._choices = self.instance.user_set.all()
+            ruf = self.fields['users_to_remove']
+            ruf.queryset = ruf._choices = self.instance.user_set.all()
 
+    def clean(self):
+        if hasattr(settings,'COURSEAFFILS_COURSESTRING_MAPPER')  \
+                and self.cleaned_data.get('course_string',False):
+            m = settings.COURSEAFFILS_COURSESTRING_MAPPER
+            stud_grp,fac_grp = m.get_groups(self.cleaned_data['course_string'])
+            if fac_grp:
+                self.cleaned_data['faculty_group'] = fac_grp
+            self.cleaned_data['group'] = stud_grp
+            return self.cleaned_data
         
+        return self.cleaned_data
+
     def clean_users_to_remove(self):
         users = self.cleaned_data['users_to_remove']
         if self.instance.group_id:
@@ -42,9 +58,12 @@ class CourseAdminForm(forms.ModelForm):
 
         #take it from here, in case instance is not yet created
         group = self.cleaned_data['group']
-
         for line in usernames.split('\n'):
             username = line.strip().rstrip()
+            also_faculty = False
+            if username.startswith('*'):
+                username = username[1:]
+                also_faculty = True
             if username:
                 try:
                     user = User.objects.get(username=username)
@@ -52,5 +71,7 @@ class CourseAdminForm(forms.ModelForm):
                     user = User(username=username)
                     user.save()
                 user.groups.add(group)
+                if also_faculty:
+                    user.groups.add(self.cleaned_data['faculty_group'])
         return usernames
 
