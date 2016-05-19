639b9046348293e9ba17d994f0c196b6f74065ff
Nik Nyby <nnyby@columbia.edu>
2016-05-19 16:31:52 -0400

    add Affil admin pages

--- courseaffils/admin.py
+++ courseaffils/admin.py
@@ -1,8 +1,10 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from courseaffils.models import Course, CourseSettings
-from courseaffils.models import CourseInfo, CourseDetails
+from courseaffils.models import (
+    Affil,
+    Course, CourseSettings, CourseInfo, CourseDetails
+)
 from courseaffils.forms import CourseAdminForm
 
 
@@ -14,6 +16,7 @@ class CourseAdmin(admin.ModelAdmin):
     change_form_template = "courseaffils/admin_change_form.html"
 
 admin.site.register(Course, CourseAdmin)
+admin.site.register(Affil)
 admin.site.register(CourseSettings)
 admin.site.register(CourseInfo)
 admin.site.register(CourseDetails)
