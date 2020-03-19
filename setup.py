503bf14052e8e3ac01eafa20ad8250cf0da69721
Nikolas Nyby <nikolas@gnu.org>
2020-03-19 17:42:17 -0400

    :bookmark: release version 2.2.3

--- setup.py
+++ setup.py
@@ -1,26 +1,25 @@
 from setuptools import setup, find_packages
 
-version = '2.2.2'
+version = '2.2.3'
 
 
-setup(name='django-courseaffils',
-      version=version,
-      description="course affiliations",
-      long_description="a django app which manages course information",
-      classifiers=[],
-      keywords='',
-      author='Columbia University\'s Center for Teaching and Learning',
-      author_email='ctl-dev@columbia.edu',
-      url='https://github.com/ccnmtl/django_courseaffils/',
-      license='',
-      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-      include_package_data=True,
-      zip_safe=False,
-      dependency_links=[
-        ],
-      install_requires=[
+setup(
+    name='django-courseaffils',
+    version=version,
+    description="course affiliations",
+    long_description="a django app which manages course information",
+    classifiers=[],
+    keywords='',
+    author='Columbia University\'s Center for Teaching and Learning',
+    author_email='ctl-dev@columbia.edu',
+    url='https://github.com/ccnmtl/django_courseaffils/',
+    license='',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
         "Django",
-      ],
-      entry_points="""
+    ],
+    entry_points="""
       """,
-      )
+)
